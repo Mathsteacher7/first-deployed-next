0f4fe49368685ba04d27027a86e180d033a191bf
Daniel Cohen <danielcohen@pvh.com>
2023-06-06 18:25:49 +0100

    added the filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Table from "@/components/table/table";
+import style from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
+      <h1 className={style.title}>מילון המושגים הכלכלי של The real yield</h1>
       <div>
         <Table />
       </div>
